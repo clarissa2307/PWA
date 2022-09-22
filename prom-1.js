631a7c9d5d759fcc68cc8d6e9b20d42818423f7f
Clarissa2307 <marthaleo07@gmail.com>
2022-09-22 18:18:06 -0500

    usando promesas

--- prom-1.js
+++ prom-1.js
@@ -15,19 +15,18 @@ function sumaUno( numero, callback ){
 sumaUno(5, function(error, nuevoValor ){
     if( error ) {
         console.log(error);
-        return;
+        return
     }
     sumaUno( nuevoValor, function(error,  nuevoValor2 ) {
     if( error ) {
-        console.log(error);
-        return;
-    }
+    console.log(error);
+    return
+}
         sumaUno(nuevoValor2, function(error, nuevoValor3){
     if( error ) {
-        console.log(error);
-        return;
-    }            
-    console.log(nuevoValor2);
+    console.log(error);
+    return
+}            console.log(nuevoValor2);
         });
         
     });
