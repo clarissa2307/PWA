c0891def258f12fb7bd01f12d7d621bf41de9a57
Clarissa2307 <marthaleo07@gmail.com>
2022-09-22 22:13:26 -0500

    errores en las promesas

--- prom-2.js
+++ prom-2.js
@@ -1,22 +1,31 @@
-function sumarUno( numero ) {
-    var promesa = new Promise( function( resolve, reject ) {
-        setTimeout( function() {
-            resolve( numero + 1);
+function sumarUno(numero) {
+    var promesa = new Promise(function (resolve, reject) {
+
+        console.log(numero);
+        if (numero >= 7 ) {
+            reject(' El numero es mas alto');
+        }
+        setTimeout(function () {
+
+
+            resolve(numero + 1);
         }, 800);
 
     });
 
-        return promesa;
+    return promesa;
 }
 
-sumarUno( 5 ).then(  nuevoNumero => {
-    console.log(nuevoNumero);
-    return sumarUno( nuevoNumero );   
-})
-    .then( nuevoNumero  => {
-    return sumarUno(nuevoNumero);
-})
-    .then( nuevoNumero =>   {
+sumarUno(5)
+    .then(sumarUno)
+    .then(sumarUno)
+    .then(sumarUno)
+    .then( nuevoNumero => {
         console.log(nuevoNumero);
-});
+    })
+    .catch( error  => {
+        console.log('ERROR EN PROMESA');
+        console.log(error);
+    });
+    
 
