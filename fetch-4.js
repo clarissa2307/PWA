d6760755cfac464fe479e2e72f04126d444b8965
Clarissa2307 <marthaleo07@gmail.com>
2022-09-25 13:03:16 -0500

    Fetch blob

---
+++ fetch-4.js
@@ -0,0 +1,11 @@
+//peticion Get
+//https://reqres.in/api/users
+let img = document.querySelector('img');
+fetch('superman.png')
+    .then( resp => resp.blob())
+    .then(  imagen  => {
+
+       // console.log(imagen);
+       var imgPath = URL.createObjectURL(imagen);
+       img.src = imgPath;
+    });
