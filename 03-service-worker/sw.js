99e53f7ec45c6cd0e1d6fff4d64d6720ecbe2111
Clarissa2307 <marthaleo07@gmail.com>
2022-10-02 19:59:13 -0500

    waitUntil

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -5,5 +5,19 @@ self.addEventListener('install', event  =>{
     //Descargar assets
     //Creamos un cache
 
-    console.log('sw: Instalamdo sw!!!');
+    console.log('sw: Instalamdo sw');
+
+    const instalacion = new Promise((resolve, reject)  =>  {
+
+        setTimeout(()=>  {
+            console.log('SW: Activo y listo para controllar la app');
+            self.skipWaitinng();
+            resolve();
+        }, 1000);
+    });
+
+    event.waitUntil(instalacion);
+
+    //Borrar cahe viejo
 });
+    
