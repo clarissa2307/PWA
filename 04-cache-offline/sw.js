80b5e69dd5ca7bd862bfc7fcfa6f4ffca79f4a39
Clarissa2307 <marthaleo07@gmail.com>
2022-10-16 12:05:00 -0500

    Network with cache

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,8 +1,18 @@
 const CACHE_NAME = 'cache-1';
-const CACHE_STATIC_NAME  = 'static-v1';
+const CACHE_STATIC_NAME = 'static-v2';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+function limpiarCache( cacheName, numeroItems ) {
+    cache.opens( cacheName ).then (cache => {
+        return cache.keys().then( keys => {
+            if (keys.length > numeroItems) {
+                cache.delete( keys[0] ).then( limpiarCache(cacheName, numeroItems) );
+            }
+        });
+    });
+}
 
 self.addEventListener('install', e => {
     const cacheProm = caches.open(CACHE_STATIC_NAME).then( cache => {
@@ -36,6 +46,7 @@ const respuesta = caches.match( e.request )
     fetch( e.request ).then( newResp => {
         caches.open(CACHE_DYNAMIC_NAME).then( cache => {
             cache.put(e.request, newResp);
+            limpiarCache( CACHE_DYNAMIC_NAME, 5);
         });
 
         return newResp.clone();
