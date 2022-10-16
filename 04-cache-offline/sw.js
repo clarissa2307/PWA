17959cc39ad94179250ba8a7e737648c2f0f0b16
Clarissa2307 <marthaleo07@gmail.com>
2022-10-16 11:16:27 -0500

    app shell sw

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,33 +1,16 @@
+self.addEventListener('install', e => {
 
-self.addEventListener('fetch', event => {
-    //const offlineResp = new Response(`
-    //    Bienvenido a mi pagina web
-    //    Disculpa, pero para usarla, necesitas internet
-    //`);
+    const cacheProm = caches.open('cache-1').then( cache => {
 
-   // const offlineResp = new Response(`
-     //<!DOCTYPE html>
-     //<html lang="en">
-     //<head>
-         //<meta charset="UTF-8">
-         //<meta name="viewport" content="width=device-width, initial-scale=1.0">
-         //<meta http-equiv="X-UA-Compatible" content="ie=edge">
-         //<title>Mi PWA</title>
-     //</head>
-     //<body class="container p-3">
-     //<h1>Offline</h1>
-     //</body>
-     //</html>
-     //`,{
-      //   headers: {
-       //      'Content-Type':'text/html'
-       //  }
-    // });
-    const offlineResp = fetch( 'pages/offline.html' );
-    const resp = fetch(event.request)
-        .catch(() => offlineResp);
+        return cache.addAll([
+            '/index.html',
+            '/css/style.css',
+            '/img/main.jpg',
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+            '/js/app.js'
+        ]);
 
-    event.respondWith(resp);
+    });
+
+    e.waitUntil(cacheProm);
 });
-
-
