12872b1108757340dc01c78d97019211820ec092
Clarissa2307 <marthaleo07@gmail.com>
2022-10-21 18:37:46 -0500

    IndexdDB

--- 07-indexeddb/app.js
+++ 07-indexeddb/app.js
@@ -1,6 +1,19 @@
 
 // indexedDB: Reforzamiento
 
+let request = window.indexedDB.open('mi-database', 1);
 
+// Se actualiza cuando se crea o se sube de version de la DB
+request.onupgradeneeded = event => {
+
+    console.log('Actualización de BD');
+
+    let db = event.target.result;
+
+    db.createObjectStore('heroes', {
+        keyPath: 'id'
+    });
+
+};
 
 
