d01153f6f04b84074a5736485ee4871c6c3f7665
Clarissa2307 <marthaleo07@gmail.com>
2022-10-27 18:19:28 -0500

    get mensaje

--- 10-twittor-offline-posting/server/routes.js
+++ 10-twittor-offline-posting/server/routes.js
@@ -3,6 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 
+const mensajes = [
+
+  {
+    _id: 'XXX',
+    user: 'spiderman',
+    mensaje: 'Hola Mundo'
+  }
+
+];
 
 
 
@@ -10,8 +19,10 @@ var router = express.Router();
 
 // Get mensajes
 router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+  // res.json('Obteniendo mensajes');
+  res.json( mensajes );
 });
+
 
 
 
