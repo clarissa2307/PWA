10f7e50d191b9b6d614afc13070eb1bced9351e4
Clarissa2307 <marthaleo07@gmail.com>
2022-10-27 22:49:23 -0500

    cambios

--- 10-twittor-offline-posting/public/js/app.js
+++ 10-twittor-offline-posting/public/js/app.js
@@ -177,3 +177,33 @@ function getMensajes() {
 
 }
 getMensajes();
+
+
+// Detectar cambios de conexión
+function isOnline() {
+
+    if ( navigator.onLine ) {
+        // tenemos conexión
+        // console.log('online');
+        $.mdtoast('Online', {
+            interaction: true,
+            interactionTimeout: 1000,
+            actionText: 'OK!'
+        });
+
+
+    } else{
+        // No tenemos conexión
+        $.mdtoast('Offline', {
+            interaction: true,
+            actionText: 'OK',
+            type: 'warning'
+        });
+    }
+
+}
+
+window.addEventListener('online', isOnline );
+window.addEventListener('offline', isOnline );
+
+isOnline();
