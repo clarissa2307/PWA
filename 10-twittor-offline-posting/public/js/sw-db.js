6f837d2b39fd0d26159c50f7f189817002b923e8
Clarissa2307 <marthaleo07@gmail.com>
2022-10-27 21:49:54 -0500

    Post y almacenamiento indexdDB

---
+++ 10-twittor-offline-posting/public/js/sw-db.js
@@ -0,0 +1,12 @@
+const db = new PouchDB('mensajes');
+function guardarMensaje( mensaje ) {
+
+    mensaje._id = new Date().toISOString();
+
+    db.put( mensaje ).then( () => {
+
+        console.log('Mensaje guardados');
+
+    });
+
+}
