a775ab8d1bea054ef9c4a51b32aabb2c833f7868
Clarissa2307 <marthaleo07@gmail.com>
2022-11-17 19:20:19 -0600

    Sucripcion

--- 11-twittor-con-push/public/js/app.js
+++ 11-twittor-con-push/public/js/app.js
@@ -2,7 +2,7 @@
 var url = window.location.href;
 var swLocation = '/twittor/sw.js';
 
-
+var swReg;
 if ( navigator.serviceWorker ) {
 
 
@@ -11,7 +11,16 @@ if ( navigator.serviceWorker ) {
     }
 
 
-    navigator.serviceWorker.register( swLocation );
+    window.addEventListener('load', function() {
+
+        navigator.serviceWorker.register( swLocation ).then( function(reg){
+
+            swReg = reg;
+            swReg.pushManager.getSubscription().then(verificaSuscripcion);
+
+        });
+
+    });
 }
 
 
@@ -233,7 +242,7 @@ function verificaSuscripcion( activadas ) {
 
 }
 
-verificaSuscripcion();
+
 
 
 function enviarNotificacion() {
@@ -298,4 +307,26 @@ function getPublicKey() {
 
 }
 
-getPublicKey().then( console.log );
+// getPublicKey().then( console.log );
+btnDesactivadas.on( 'click', function() {
+
+    if ( !swReg ) return console.log('No hay registro de SW');
+
+    getPublicKey().then( function( key ) {
+
+        swReg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: key
+        })
+        .then( res => res.toJSON() )
+        .then( suscripcion => {
+
+            console.log(suscripcion);
+
+            verificaSuscripcion(suscripcion);
+
+        });
+
+    });
+
+});
